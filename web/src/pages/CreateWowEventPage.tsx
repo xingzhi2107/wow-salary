0ae995b6b506ff7e619431f0919cb2122f4e5bfb
Xingzhi <xingzhi@petalmail.com>
2021-12-16 13:53:05 +0800

    web, integrate axios

--- web/src/pages/CreateWowEventPage.tsx
+++ web/src/pages/CreateWowEventPage.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {Button} from 'antd';
 import {Link} from 'react-router-dom';
+import {fetchWcl} from '../apis';
 
-export function CreateWowEventPage() {
-  return (
-    <div>
-      <Link to="/login">{'登录'}</Link>
-      <Button type="primary">{'提交'}</Button>
-    </div>
-  );
+interface Props {}
+
+interface State {
+  wcl: string;
+}
+
+export class CreateWowEventPage extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      wcl: '',
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to="/login">{'登录'}</Link>
+        <Button type="primary">{'提交'}</Button>
+        <div style={{whiteSpace: 'pre'}}>{this.state.wcl}</div>
+      </div>
+    );
+  }
+
+  loadData = () => {
+    fetchWcl('GjmB9Q7rfzhPck6A').then(res => {
+      this.setState({
+        wcl: JSON.stringify(res.data.reportData.report, null, 4),
+      });
+    });
+  };
 }
